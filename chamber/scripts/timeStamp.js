05206da61d0ddf78ab64e714889e66a538bb57db
Nephi-Brown <nephi1brown@gmail.com>
2025-05-31 15:40:17 +0100

    Delete chamber/scripts/timeStamp.js

--- chamber/scripts/timeStamp.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const timeStampField = document.getElementById("timeStamp");
-    if (timeStampField) {
-      const now = new Date();
-      timeStampField.value = now.toISOString();
-    }
-  });
