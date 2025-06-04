7596c84e3660a6f9f8f01916638fd4ec3920439f
Nephi-Brown <nephi1brown@gmail.com>
2025-06-04 16:08:09 +0100

    Add files via upload

--- chamber/scripts/modebutton.js
+++ chamber/scripts/modebutton.js
@@ -1,18 +1,31 @@
 const modeButton = document.querySelector("#mode");
+const modeIcon = document.querySelector("#modeIcon");
 const main = document.querySelector("main");
 const h1 = document.querySelector("h1");
 
 modeButton.addEventListener("click", () => {
-	main.classList.toggle("dark");
+main.classList.toggle("dark");
 
-	if (main.classList.contains("dark")) {
-		main.style.background = "#9932CC";
-		h1.style.color = "#FFFFFF";
-		modeButton.innerHTML = "🔆";
-	}
-	else {
-		main.style.background = "#FFFFFF";
-		h1.style.color = "#222222"; // 
-		modeButton.innerHTML = "◐";
-	}
+
+const isDarkMode = main.classList.contains("dark");
+
+  main.style.background = isDarkMode ? "#4169E1" : "#FFFFFF";
+  h1.style.color = isDarkMode ? "#FFFFFF" : "#222222";
+  modeIcon.textContent = isDarkMode ? "🔆" : "◐";
+});
+
+const modebutton = document.querySelector("#mode");
+const modeicon = document.querySelector("#modeIcon");
+const main = document.querySelector("main");
+const h1 = document.querySelector("h1");
+
+modeButton.addEventListener("click", () => {
+main.classList.toggle("dark");
+
+
+const isDarkMode = main.classList.contains("dark");
+
+  main.style.background = isDarkMode ? "#4169E1" : "#FFFFFF";
+  h1.style.color = isDarkMode ? "#FFFFFF" : "#222222";
+  modeIcon.textContent = isDarkMode ? "🔆" : "◐";
 });
